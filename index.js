4bfefdf749044d132e6bc714c28c640d623ebf5d
Amro dandashli <amrdandashli@gmail.com>
2021-04-30 02:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+ 
+]
+// function superbowlWin(record){
+//   record.find()
+// }const inventory = [
+//   {name: 'apples', quantity: 2},
+//   {name: 'bananas', quantity: 0},
+//   {name: 'cherries', quantity: 5}
+// ];
+
+const result = record.find( ({ year }) => if(year === 'w')=>year );
